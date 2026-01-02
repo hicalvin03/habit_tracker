1ac789d1eaf43704da038062821262e429ec0c86
Calvin lee <hicalvin03@gmail.com>
2026-01-03 00:19:31 +1100

    improved design/ui of the todolist

---
+++ src/components/checkbox.jsx
@@ -0,0 +1,25 @@
+import './checkbox.css'
+
+function CustomCheckBox() {
+    return (
+        <div className="checkbox-wrapper">
+            <input type="checkbox" className="check" id="check1-61" />
+            <label htmlFor="check1-61" className="label">
+                <svg width="45" height="45" viewBox="0 0 95 95">
+                    <rect x="30" y="20" width="50" height="50" stroke="black" fill="none" />
+                    <g transform="translate(0,-952.36222)">
+                        <path 
+                            d="m 56,963 c -102,122 6,9 7,9 17,-5 -66,69 -38,52 122,-77 -7,14 18,4 29,-11 45,-43 23,-4" 
+                            stroke="black" 
+                            strokeWidth="3" 
+                            fill="none" 
+                            className="path1" 
+                        />
+                    </g>
+                </svg>
+            </label>
+        </div>
+    );
+}
+
+export default CustomCheckBox
